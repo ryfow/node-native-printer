79c3eac69642d09f512e166aa50140161c36d52c
Ryan Fowler <ryan.fowler@singlewire.com>
2023-05-16 13:21:16 -0600

    remove the rest of the install code

--- install/install_win.js
+++ install/install_win.js
@@ -1,12 +1,3 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
-const dotenv = require('dotenv');
-
 module.exports = function(){
-	makeEnv("edge");
 };
-
-function makeEnv(pack) {
-	fs.writeFileSync(fs.realpathSync(__dirname + '\\..') + "\\.env", `NNP_PACKAGE=${pack}`);
-}
 
